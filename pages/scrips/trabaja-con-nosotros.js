16936b2af23f071be7c4585512fe52880e30e0de
Lautaro Tobares <lautarotobares282@gmail.com>
2023-12-17 10:18:31 -0300

    añadiendo js

---
+++ pages/scrips/trabaja-con-nosotros.js
@@ -0,0 +1,31 @@
+document.getElementById('enviarBtn').addEventListener('click', function(event) {
+    event.preventDefault(); 
+  
+    // obtiene los valores dados por el usuario
+    let nombre = document.getElementById('nombre').value;
+    let apellido = document.getElementById('apellido').value;
+    let email = document.getElementById('email').value;
+    let acepto = document.getElementById('acepto').checked;
+  
+    //validaciones
+    if (nombre === '' || apellido === '' || email === '' || !acepto) {
+      alert('Por favor, completa todos los campos y acepta los términos y condiciones.');
+    } else {
+      // crea 'PostulantesBoreal' y almacena los datos
+      let postulante = {
+        nombre: nombre,
+        apellido: apellido,
+        email: email
+      };
+  
+      // Mostrar en consola
+      console.log('Postulantes Boreal:');
+      console.log(postulante);
+
+      alert('Gracias elegirnos. En los próximos días te contactaremos al correo brindado.');
+
+  
+      
+      document.getElementById('miFormulario').reset(); 
+    }
+  });
